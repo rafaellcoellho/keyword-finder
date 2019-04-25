c7a9dbd7a1cd8a61ed283901f7486c8c658e593e
Rafael Coelho <rafaellcoellho@gmail.com>
2019-04-25 08:59:25 -0300

    Changed to sanic to make things better

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic()
 
 @app.route("/")
-def hello():
-  return "Hello World!"
+async def test(request):
+    return json({"hello": "world"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
   
