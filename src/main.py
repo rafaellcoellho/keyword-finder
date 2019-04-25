ad8658b78f23190e7aa0b60971655fd2d60e0af0
Rafael Coelho <rafaellcoellho@gmail.com>
2019-04-25 10:27:59 -0300

    Baby steps...

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from sanic import Sanic
-from sanic.response import json
+from sanic.response import json,text
 
 app = Sanic()
 
-@app.route("/")
-async def test(request):
-    return json({"hello": "world"})
+@app.route("/alive")
+async def running(req):
+    return text("RUNNING")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="127.0.0.1", port=8000)
   
