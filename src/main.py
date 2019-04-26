64e38d549194b06a2e472b26e4019cd8fec17abb
Rafael Coelho <rafaellcoellho@gmail.com>
2019-04-26 10:38:26 -0300

    Add swagger docs

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from aiohttp import web
+from aiohttp_swagger import setup_swagger
 
 routes = web.RouteTableDef()
 
@@ -8,10 +9,24 @@ async def hello(request):
 
 @routes.get('/alive')
 async def alive(request):
+    """
+    ---
+    description: This end-point allow to test that service is up.
+    tags:
+    - Health check
+    produces:
+    - text/plain
+    responses:
+        "200":
+            description: successful operation. Return "RUNNING" text
+        "405":
+            description: invalid HTTP Method
+    """
     return web.Response(text="RUNNING")
 
 async def main(): 
   app = web.Application()
   app.add_routes(routes)
+  setup_swagger(app, swagger_url="/docs")
   return app
 
