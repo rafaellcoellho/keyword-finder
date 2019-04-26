d23f47b9b10bab5e7536202dca97c51159f4953f
Rafael Coelho <rafaellcoellho@gmail.com>
2019-04-26 13:39:13 -0300

    Receive parameters in body and return ex json

--- src/main.py
+++ src/main.py
@@ -7,42 +7,18 @@ routes = web.RouteTableDef()
 
 @routes.post('/')
 async def crawler(request):
-    """
-    ---
-    description: Conta o número de ocorrencias de uma palavra em urls
-    tags:
-    - Crawler
-    produces:
-    - application/json
-    responses:
-        "200":
-            description: Sucesso. Retorna json com os resultados
-        "405":
-            description: Método Http inválido
-        "500":
-            description: Erro interno
-    """
     req = json.loads(await request.content.read())
     word = req['word']
     links = req['urls']
-    print(word, "\n",links)
-    return web.Response(text="Huehue")
+    
+    res = {}
+    for link in links:
+        res[link] = 0
+    
+    return web.json_response(res)
 
 @routes.get('/alive')
 async def alive(request):
-    """
-    ---
-    description: Testar se o serviço está funcionando
-    tags:
-    - Testes
-    produces:
-    - text/plain
-    responses:
-        "200":
-            description: Está funcionando. Retorna "RUNNING"
-        "405":
-            description: Método Http inválido
-    """
     return web.Response(text="RUNNING")
 
 async def main(): 
@@ -51,6 +27,6 @@ async def main():
   app = web.Application()
   app.add_routes(routes)
   if env == 'development':
-      setup_swagger(app, swagger_url="/docs")
+      setup_swagger(app, swagger_url="/docs", swagger_from_file="docs/main.yaml")
   return app
 
