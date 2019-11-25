d4e9f5dfbb671b0d154d828d31786efc117182bc
Rafael Coelho <rafaellcoellho@gmail.com>
2019-11-25 17:35:52 -0300

    .

--- app/main.py
+++ app/main.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
+
+class PageSearchRequest(BaseModel):
+    word: str
+    urls: List[str]
+
+
+class SearchResult(BaseModel):
+    word: str
+    url: str
+
+
+class PageSearchResponse(BaseModel):
+    search: List[SearchResult]
+
+
 app = FastAPI()
 
 
 @app.get("/alive", status_code=200)
 async def alive():
     return {"text": "RUNNING"}
+
+@app.post("/", status_code=200)
+async def crawler(req: PageSearchRequest):
+    
+    return {
+        "rsarch"
+    }
--- tests/test_main.py
+++ tests/test_main.py
@@ -8,3 +8,16 @@ def test_alive():
     res = client.get("/alive")
     assert res.status_code == 200
     assert res.json() == {"text": "RUNNING"}
+
+
+def test_search_word():
+    example_search = {
+        "word": "python",
+        "urls": ["https://www.python.org/"]
+    }
+    res = client.post("/", json=example_search)
+    assert res.json() == {
+        "search": [
+            {"word": "python", "count": 1, "url": "https://www.python.org/"}
+        ]
+    }
